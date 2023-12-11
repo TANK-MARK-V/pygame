113f4e4dd08804987c419d18e453d6ed7bfe83ed
Carlsonio <145243065+Carlsonio@users.noreply.github.com>
2023-12-11 16:16:38 +0700

    Made escaping in  pygame.kir.py.

--- pygame.kir.py
+++ pygame.kir.py
@@ -14,11 +14,12 @@ if __name__ == "__main__":
     dscr = draw_death_screen(screen)
     screen.blit(dscr, (20, 200))
     pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
+    esc_flag = False
+    while not esc_flag:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-            elif event.type == pygame.KEYDOWN:
+                esc_flag = True
+            if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
+                    esc_flag = True
     pygame.quit()
