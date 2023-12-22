936b79a3ee9d9e0bcd817d769c179ed78de1ed10
TANK-MARK-V <dvornikov.mark@mail.ru>
2023-12-22 23:41:16 +0700

    Можно прыгать по блокам, есть анимация игрока. Нужно сделать спавн врагов, босса, возможность удара, вохможность убрать блоки для запрыга

--- class_room.py
+++ class_room.py
@@ -19,11 +19,11 @@ class Room:  # Класс для основной игры
         pygame.draw.rect(self.screen, grass,
                          ((bloks[0][0], bloks[0][1]), (bloks[0][2] - bloks[0][0], bloks[0][3] - bloks[0][1])), 0)
         pygame.draw.rect(self.screen, grass,
-                         ((bloks[2][0], bloks[2][1]), (bloks[2][2] - bloks[2][0], bloks[2][3] - bloks[2][1])), 0)
+                         ((bloks[1][0], bloks[1][1]), (bloks[1][2] - bloks[1][0], bloks[1][3] - bloks[1][1])), 0)
         pygame.draw.rect(self.screen, grass,
-                         ((bloks[4][0], bloks[4][1]), (bloks[4][2] - bloks[4][0], bloks[4][3] - bloks[4][1])), 0)
-        if park:  # Если игрок добился того, что к боссу можно допрыгнуть
-            pygame.draw.rect(self.screen, grass,
-                             ((bloks[1][0], bloks[1][1]), (bloks[1][2] - bloks[1][0], bloks[1][3] - bloks[1][1])), 0)
-            pygame.draw.rect(self.screen, grass,
-                             ((bloks[3][0], bloks[3][1]), (bloks[3][2] - bloks[3][0], bloks[3][3] - bloks[3][1])), 0)
+                         ((bloks[2][0], bloks[2][1]), (bloks[2][2] - bloks[2][0], bloks[2][3] - bloks[2][1])), 0)
+        # if park:  # Если игрок добился того, что к боссу можно допрыгнуть
+        #     pygame.draw.rect(self.screen, grass,
+        #                      ((bloks[1][0], bloks[1][1]), (bloks[1][2] - bloks[1][0], bloks[1][3] - bloks[1][1])), 0)
+        #     pygame.draw.rect(self.screen, grass,
+        #                      ((bloks[3][0], bloks[3][1]), (bloks[3][2] - bloks[3][0], bloks[3][3] - bloks[3][1])), 0)
